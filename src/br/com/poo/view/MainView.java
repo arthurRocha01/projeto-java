96c8c92ee94056e39d83994409fb32252bf620c6
ARTHUR DA ROCHA SANTOS <023.916783@universidadesenaicimatec.edu.br>
2025-06-05 15:38:13 -0300

    reajuste

--- src/br/com/poo/view/MainView.java
+++ src/br/com/poo/view/MainView.java
@@ -44,7 +44,7 @@ public class MainView extends JFrame {
 			int opcaoSelecionada = this.promptViewFunctions.menuSelecao("INCIAL", opcoes, 1);
 			if (opcaoSelecionada == 1) new PromotorViewPrompt(this.controller);
 			if (opcaoSelecionada == 2) new ClientViewPrompt(this.controller);
-			break;
+			if (opcaoSelecionada == -1) break;
 		}
 		this.promptViewFunctions.exibirTelaSaida();
 	}
